b5760260fb418f23839b14369921a0540dbd453c
Satanw8you <trusnikolaj41@gmail.com>
2024-03-15 17:40:38 +0200

    some edits

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Feedback from "../Feedback/Feedback";
 import Options from "../Options/Options";
 import Notification from "../Notification/Notification";
-import css from "./App.module.css";
+import Description from "../Description/Description";
 
 const reviews = {
   good: 0,
@@ -26,13 +26,12 @@ const App = () => {
     localStorage.setItem("reviews", JSON.stringify(reviewsValue));
   }, [reviewsValue]);
   const totalFeedback = reviewsValue.good + reviewsValue.neutral + reviewsValue.bad;
-  const totalFeedbackPercentage = Math.round(((reviewsValue.good + reviewsValue.neutral) / totalFeedback) * 100);
+  const positiveFeedbackPercentage = Math.round(((reviewsValue.good + reviewsValue.neutral) / totalFeedback) * 100);
   return (
     <div>
-      <h1 className={css.title}>Sip Happens Café</h1>
-      <p className={css.text}>Please leave your feedback about our service by selecting one of the options below.</p>
+      <Description />
       <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} resetFeedback={resetFeedback} />
-      {totalFeedback === 0 ? <Notification /> : <Feedback reviewsValue={reviewsValue} feedbackPercentage={totalFeedbackPercentage} />}
+      {totalFeedback === 0 ? <Notification /> : <Feedback reviewsValue={reviewsValue} feedbackPercentage={positiveFeedbackPercentage} />}
     </div>
   );
 };
